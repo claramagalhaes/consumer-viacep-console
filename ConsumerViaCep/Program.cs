a0fca626e9d79875efbdc257e0604152bc063308
Maria Clara Martins <134343466+claramagalhaes@users.noreply.github.com>
2025-10-01 23:23:11 +0000

    atualização

---
+++ ConsumerViaCep/Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsumerViaCep.Models;
+using static System.Console;
+
+WriteLine("Digite o seu CEP:");
+var cep = ReadLine();
+
+var enderecoUrl = $@"https://viacep.com.br/ws/{cep}/json/";
+
+var client = new HttpClient();
+
+try
+{
+    HttpResponseMessage respostaApi = await client.GetAsync(enderecoUrl);
+
+    respostaApi.EnsureSuccessStatusCode();
+
+    string respostaApiJson = await respostaApi.Content.ReadAsStringAsync();
+
+    Endereco? endereco = System.Text.Json.JsonSerializer.Deserialize<Endereco>(respostaApiJson);
+
+    WriteLine("CEP:\n" + endereco.cep);
+    WriteLine("Logradouro:\n" + endereco.logradouro);
+    WriteLine("Complemento:\n" + endereco.complemento);
+
+}
+catch (Exception ex)
+{
+    WriteLine($"Aconteceu um erro:\n {ex.Message}");
+}
